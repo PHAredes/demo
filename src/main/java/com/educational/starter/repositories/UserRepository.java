f7a20183955c327c422a76b6f041ad28eaca6741
PHAredes <pedro.aredes@hotmail.com>
2022-06-28 09:43:17 -0300

    Service layer and component registration added plus necessary changes at UserResources and UserRepository to implement those change; initial tests on Postman

--- src/main/java/com/educational/starter/repositories/UserRepository.java
+++ src/main/java/com/educational/starter/repositories/UserRepository.java
@@ -2,5 +2,7 @@ package com.educational.starter.repositories;
 
 import com.educational.starter.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+//no need to tag as it extends from JPARepository meaning it is already a @Repository
 public interface UserRepository extends JpaRepository<User, Long> {}
